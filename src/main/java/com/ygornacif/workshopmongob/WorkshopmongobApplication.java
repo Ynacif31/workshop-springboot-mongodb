11e417e49b79efe459105db524a254ad08453f83
Ygor Nacif <ygor.nacif00@gmail.com>
2024-09-30 16:35:41 -0300

    Criando o projeto

---
+++ src/main/java/com/ygornacif/workshopmongob/WorkshopmongobApplication.java
@@ -0,0 +1,13 @@
+package com.ygornacif.workshopmongob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygornacif/workshopmongob/WorkshopmongobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygornacif.workshopmongob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
