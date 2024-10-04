5c0fdf2179a10f3dff4ae93edaaadf4a5db1e128
Ygor Nacif <ygor.nacif00@gmail.com>
2024-10-04 17:03:23 -0300

    insert

--- src/main/java/com/ygornacif/workshopmongob/service/UserService.java
+++ src/main/java/com/ygornacif/workshopmongob/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ygornacif.workshopmongob.service;
 
 import com.ygornacif.workshopmongob.domain.User;
+import com.ygornacif.workshopmongob.dto.UserDTO;
 import com.ygornacif.workshopmongob.exception.ObjectNotFoundException;
 import com.ygornacif.workshopmongob.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
